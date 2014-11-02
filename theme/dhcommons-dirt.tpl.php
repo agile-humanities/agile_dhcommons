032f40bc54d2d324566f799983e1c39ea98d5e92
Alan Stanley <astanley@upei.ca>
2014-11-02 15:51:57 -0400

    initial commit

---
+++ theme/dhcommons-dirt.tpl.php
@@ -0,0 +1,8 @@
+<?php
+
+?>
+<div>
+  <p class ="dirt_title"><?php print $title; ?></p>
+  <p class ="dirt_description"><?php print $description; ?></p>
+  <p class ="dirt_description"><?php print $link; ?></p>
+</div>
